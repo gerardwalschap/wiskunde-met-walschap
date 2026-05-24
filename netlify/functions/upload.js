01de60ffdeb9a9d0c9226c53bf4d669ffd2f0334
gerardwalschap <gerard.walschap@telenet.be>
2026-05-24 07:26:39 +0200

    Update upload.js

--- netlify/functions/upload.js
+++ netlify/functions/upload.js
@@ -4,18 +4,12 @@ exports.handler = async (event) => {
   }
 
   try {
-    const body = JSON.parse(event.body);
-    const { path, fileBase64, fileName } = body;
-
     const { getStore } = require('@netlify/blobs');
-    const store = getStore('bestanden');
-    const fileBuffer = Buffer.from(fileBase64, 'base64');
-    await store.set(path, fileBuffer);
-
+    
     return {
       statusCode: 200,
       headers: { 'Access-Control-Allow-Origin': '*' },
-      body: JSON.stringify({ success: true, path })
+      body: JSON.stringify({ success: true, blobsLoaded: true })
     };
   } catch (e) {
     return {
